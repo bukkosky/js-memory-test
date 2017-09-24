35788a0b620cebaf891877624fff3bd44605b657
rettles <bartosz.gro@gmail.com>
2017-09-24 20:39:53 +0200

    Game update

--- js/memory.js
+++ js/memory.js
@@ -1,15 +1,44 @@
-let pictures, lock, oneVisible, pairs, turns, imgData, cards;
+let pictures, lock, oneVisible, pairs, turns, imgData, cards, score, level;
 let g = new Game();
 g.drawMenu();
 
+function Score() {
+
+    this.easy = 0;
+    this.medium = 0;
+    this.hard = 0;
+
+    this.getScore = function (level) {
+        if(level === "easy") {
+            return this.easy;
+        } else if( level === "medium") {
+            return this.medium;
+        } else {
+            return this.hard;
+        }
+    };
+
+    this.setScore = function (level, score) {
+        if (level === "easy") {
+            this.easy = score;
+        } else if (level === "medium") {
+            this.medium = score;
+        } else {
+            this.hard = score;
+        }
+    };
+}
+
 function Game() {
+
+    score = new Score;
 
     this.drawMenu = function () {
 
         let $board = $(".board");
         let levels = ["Easy", "Medium", "Hard"];
 
-        $board.html("<h2>Choose your difficulty level</h2>");
+        $board.html("<div class=\"board-header\">Choose your difficulty level</div>");
 
         for(let i = 0; i < 3; i++) {
             $board.append("<div class=\"btn-level\">" + levels[i] + "</div>");
@@ -34,21 +63,24 @@ function Game() {
         cards = [];
 
         $board.html("<div class=\"field\"></div>");
-        $field = $(".field");
+        let $field = $(".field");
 
         switch(level) {
             // easy - 4x4 - 16 cards
             case 0:
                 $field.addClass("small");
+                level = "easy";
                 aLength = 8;
                 break;
             // normal - 6x5 - 30 cards
             case 1:
+                level = "medium";
                 $field.addClass("medium");
                 aLength = 15;
                 break;
             // difficult - 6x7 - 42 cards
             case 2:
+                level = "hard";
                 $field.addClass("big");
                 aLength = 21;
                 break;
@@ -91,13 +123,25 @@ function Card(img, id) {
 
     this.hide = function () {
         let $id = $("#" + this.id);
+        let $board = $(".board");
         this.hidden = true;
 
-        $id.toggleClass("hidden");
+        $id.toggleClass("hidden highlight");
         pairs--;
 
         if(pairs === 0) {
-            $(".board").html("<h1>You win!<br>Done in " + turns + " turns</h1><a class=\"btn\">Play again</a>");
+
+            // New best score
+            if(score.getScore(level) === 0 || score.getScore(level) > turns) {
+                score.setScore(level, turns);
+                $board.html("<div class=\"score-header\">Congratulations You Won!</div>" +
+                    "<div class=\"score-new\">New record:<br> " + score.getScore(level) + " turns!</div>");
+            } else {
+                $board.html("<div class=\"score-header\">Congratulations You Won!</div>" +
+                    "<div class=\"score-new\">Your score: " + turns + "</div>" +
+                    "<div class=\"score-best\">The best score: " + score.getScore(level) + "</div>");
+            }
+            $board.append("<a class=\"btn\">Play again</a>");
             $(".btn").on("click", function () {
                 g.drawMenu();
             });
@@ -106,7 +150,7 @@ function Card(img, id) {
     this.restore = function () {
         let $id = $("#" + this.id);
 
-        $id.toggleClass("highlight");
+        $id.toggleClass("highlight rotateCard");
         $id.css({"background-image":""});
         this.revealed = false;
     };
@@ -119,6 +163,7 @@ function Card(img, id) {
             lock = true;
 
             $id.css({"background-image": this.img});
+            $id.addClass("rotateCard");
             $id.toggleClass("highlight");
 
             if(!oneVisible) {
