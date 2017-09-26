1cd4614e6713936d0da2556400222670d73794bd
rettles <bartosz.gro@gmail.com>
2017-09-27 00:09:15 +0200

    little change

--- js/memory.js
+++ js/memory.js
@@ -1,12 +1,17 @@
 let pictures, lock, oneVisible, pairs, turns, imgData, cards, score, level, timer, secs, time;
 
-$(document).ready(function () {
-    let preloadImages = [];
-    for(let i = 0; i < 21; i++) {
-       preloadImages.append(new Image());
-       preloadImages[i].src = i+".png";
-   }
-});
+let imagesToLoad = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png", "12.png",
+    "13.png", "14.png", "15.png", "16.png", "17.png", "18.png","19.png", "20.png","21.png", "card.png"];
+
+$.fn.preload = function() {
+
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+};
+
+$(imagesToLoad).preload();
+
 
 let g = new Game();
 
